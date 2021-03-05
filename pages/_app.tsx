871581a95ff1fdfd406c00388ac818c33b23d6c6
Jan Kraus <kraus.jan@gmail.com>
2021-03-05 19:38:28 +0100

    Include analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import type { AppProps /*, AppContext */ } from 'next/app';
+import * as React from 'react'
+import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-
 import { QueryClient, QueryClientProvider } from 'react-query';
+
+import * as gtag from "../utils/gtag";
+
 const queryClient = new QueryClient();
 
 import '../styles/global.scss';
@@ -12,6 +15,16 @@ import { Layout } from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+
+  React.useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <Layout meta={pageProps.meta} {...pageProps.layout} currentPath={router.pathname}>
