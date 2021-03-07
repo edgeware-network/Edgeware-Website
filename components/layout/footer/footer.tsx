7ce19b2ac193ba17e3b31f8ebf05b93b833fd9e9
Jan Kraus Stanko <krausjan@mediamarktsaturn.com>
2021-03-07 13:48:28 +0100

    Fix typo in footer Lockdrop link

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -20,7 +20,7 @@ const FOOTER_LINKS: FooterLinksGroup[] = [
     items: {
       Mission: '/mission',
       Collectives: '/collectives',
-      LockDrop: 'https://commonwealth.im/edgeware/stats',
+      Lockdrop: 'https://commonwealth.im/edgeware/stats',
       Press: '/press',
     },
   },
