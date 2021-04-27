8e1f758ff7e9bbd6e2554abd63cffde6f0754ff1
Jan Kraus <kraus.jan@gmail.com>
2021-04-27 23:02:44 +0200

    Setup structure, api, content and layout for Edgeware jobs pages

---
+++ components/pages/jobs/job-content/job-content.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+interface JobContentProps {
+  content: string
+}
+
+import styles from './job-content.module.scss';
+
+export const JobContent: React.FC<JobContentProps> = ({ content }) => {
+  return (
+    <div className={styles.content}>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  )
+}
