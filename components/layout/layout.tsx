4b3266021ebfa81d7b22d46dfef19c62c1a6e41c
jsteneros <74991457+jsteneros@users.noreply.github.com>
2021-05-29 10:52:46 +0200

    New assets and minor tweak for design refactor

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -46,9 +46,9 @@ export const Layout: React.FC<LayoutProps> = ({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#151515" />
-        <meta name="msapplication-TileColor" content="#151515" />
-        <meta name="theme-color" content="#151515" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff3b80" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
 
         <meta property="og:url" content={pageUrl} />
         <meta property="og:type" content="website" />
