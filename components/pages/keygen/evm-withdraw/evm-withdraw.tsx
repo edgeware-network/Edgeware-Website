1279d8cbc22c8fa8875c446399fd17df5bb09aac
raymondz <raykyri@gmail.com>
2021-08-29 22:17:55 -0700

    First pass at EVM withdraw

---
+++ components/pages/keygen/evm-withdraw/evm-withdraw.tsx
@@ -0,0 +1,174 @@
+import { u8aToHex } from '@polkadot/util';
+import Keyring from '@polkadot/keyring';
+import { spec } from '@edgeware/node-types';
+import { TypeRegistry } from '@polkadot/types';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { decodeAddress, encodeAddress } from '@polkadot/util-crypto';
+import Web3 from 'web3';
+import BN from 'bn.js';
+import { useRef, useState } from 'react';
+
+import { Button } from '../../../common/button/button';
+import { P } from '../../../common/typography/typography';
+
+import styles from './evm-withdraw.module.scss';
+
+// conditional import for extension-dapp
+let web3Accounts, web3Enable, web3FromAddress;
+if (typeof window !== "undefined") {
+  const extension = require('@polkadot/extension-dapp');
+  web3Accounts = extension.web3Accounts;
+  web3Enable = extension.web3Enable;
+  web3FromAddress = extension.web3FromAddress;
+}
+
+export const EvmWithdraw = () => {
+  const inputEl = useRef(null);
+  const amountEl = useRef(null);
+  const [formState, setFormState] = useState({
+    text: null,
+    error: false,
+  });
+
+  const evmToMainnet = async () => {
+    setFormState({});
+
+    const web3 = new Web3(window.ethereum);
+    const account = web3?.eth?.accounts?.currentProvider?.selectedAddress;
+    const substrateAddress = inputEl.current.value;
+    const amount = amountEl.current.value;
+    if (isNaN(+amount)) {
+      setFormState({ text: 'Invalid amount', error: true });
+      return;
+    };
+    if (!account || !substrateAddress) {
+      setFormState({ text: 'Invalid Substrate address', error: true });
+      return;
+    }
+
+    // are we on the right network?
+    if (+web3.eth.accounts.currentProvider.chainId !== 2021 &&
+        +web3.eth.accounts.currentProvider.chainId !== 2022) {
+      setFormState({ text: 'Wallet must be configured to EVM chain ID 2021 for Edgeware', error: true });
+      return;
+    }
+
+    try {
+      const addressBytes = decodeAddress(substrateAddress);
+      const evmAddress = Buffer.from(addressBytes.subarray(0, 20)).toString('hex');
+      await web3.eth.sendTransaction({
+        from: account,
+        to: evmAddress,
+        value: Web3.utils.toWei(amount),
+        gas: '55000',
+      }).on('transactionHash', (_) => {
+        setFormState({ text: 'Transaction sent, wait for confirmation...', error: true });
+      });
+      setFormState({ text: 'Transaction success, please continue to step 2.' });
+    } catch (err) {
+      if (err.code === 4001) {
+        setFormState({ text: 'Transaction canceled', error: true });
+      } else {
+        setFormState({ text: 'Transaction error', error: true });
+      }
+    }
+  };
+
+  const evmWithdraw = async () => {
+    setFormState({});
+
+    const sender = inputEl.current.value;
+    const amount = amountEl.current.value;
+    if (isNaN(+amount)) {
+      setFormState({ text: 'Invalid amount', error: true });
+      return;
+    };
+    if (!sender) {
+      setFormState({ text: 'Invalid Substrate address', error: true });
+      return;
+    }
+
+    const addressBytes = decodeAddress(sender);
+    const addressHex = u8aToHex(addressBytes);
+    const evmAddress = '0x' + Buffer.from(addressBytes.subarray(0, 20)).toString('hex');
+
+    const polkadotUrl = 'wss://mainnet.edgewa.re';
+    const registry = new TypeRegistry();
+    const api = await (new ApiPromise({
+      provider: new WsProvider(polkadotUrl),
+      registry,
+      ...spec,
+    })).isReady;
+
+    const keyring = new Keyring();
+    keyring.setSS58Format(7);
+
+    await web3Enable('Edgeware key management app');
+    const allAccounts = await web3Accounts();
+    const allAccountsHex = allAccounts.map(({ address }) => u8aToHex(keyring.decodeAddress(address)));
+    if (allAccountsHex.indexOf(addressHex) === -1) {
+      setFormState({ text: 'Address not found in wallet', error: true });
+      return;
+    }
+
+    const injector = await web3FromAddress(sender);
+    const tx = api.tx.evm.withdraw(
+      evmAddress,
+      (amount * (10 ** api.registry.chainDecimals)).toString(),
+    ).signAndSend(sender, { signer: injector.signer }, (result) => {
+      if (result.isError) {
+        setFormState({ text: 'Transaction error', error: true });
+      }
+      if (result.isCompleted) {
+        setFormState({ text: 'Transaction success. Withdrawal complete!' });
+      }
+    });
+  };
+
+  const evmCheckBalance = async () => {
+  };
+
+  return (
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label className={styles.label} htmlFor="ac-input-withdraw-address" aria-label="Address">
+          <input
+            id="ac-input-withdraw-address"
+            className={styles.input}
+            type="text"
+            name="input"
+            placeholder="Substrate address (e.g. nJrsr...)"
+            ref={inputEl}
+            autoComplete="off"
+            autoCapitalize="off"
+	    autoCorrect="off"
+          />
+          <input
+            id="ac-input-withdraw-amount"
+            className={styles.input}
+            type="text"
+            name="input"
+            placeholder="Amount (EDG)"
+            ref={amountEl}
+            autoComplete="off"
+            autoCapitalize="off"
+	    autoCorrect="off"
+          />
+        </label>
+        <div style={{ paddingRight: 10, paddingTop: 10 }}>
+          <Button onClick={evmToMainnet} style="primary-small">
+            1. Transfer to withdraw address
+          </Button>
+        </div>
+        <div style={{ paddingRight: 10, paddingTop: 10 }}>
+          <Button onClick={evmWithdraw} style="primary-small">
+            2. Withdraw from EVM
+          </Button>
+        </div>
+        <div className={formState.error ? styles.errorText : styles.successText}>
+          {formState.text}
+        </div>
+      </form>
+    </>
+  );
+};
--- pages/keygen.tsx
+++ pages/keygen.tsx
@@ -4,6 +4,7 @@ import { H1, H2, P } from '../components/common/typography/typography';
 import { Section } from '../components/common/section/section';
 import { KeyPairGenerator } from '../components/pages/keygen/keypair-generator/keypair-generator';
 import { AddressConverter } from '../components/pages/keygen/address-converter/address-converter';
+import { EvmWithdraw } from '../components/pages/keygen/evm-withdraw/evm-withdraw';
 
 export default function Keygen() {
   return (
@@ -19,6 +20,30 @@ export default function Keygen() {
             air-gapped computer.
           </P>
         </div>
+      </Section>
+
+      <Section id="convert-evm" width="normal" gap="standard">
+        <H2>Deposit to EVM</H2>
+        <P>
+          If you have an Metamask address (e.g. 0x1234...), this tool converts it
+          into a mainnet address (e.g. i76Ux...) where you can send balances from polkadot-js.
+        </P>
+
+        <AddressConverter type="evm-address" />
+      </Section>
+
+      <Section id="withdraw-evm" width="normal" gap="standard">
+        <H2>Withdraw from EVM</H2>
+        <P>
+          To withdraw EDG from Metamask to polkadot-js, first send it to the withdraw
+          address which corresponds to your polkadot-js address. Then, execute an
+          withdraw transaction from that address.
+        </P>
+        <P>
+          Note: This requires both Metamask (or another compatible wallet) and polkadot-js.
+        </P>
+
+        <EvmWithdraw />
       </Section>
 
       <Section id="keypair" width="normal" gap="standard">
@@ -52,16 +77,6 @@ export default function Keygen() {
         </P>
 
         <AddressConverter type="ss58-address" />
-      </Section>
-
-      <Section id="convert-evm" width="normal" gap="standard">
-        <H2>Convert Metamask/EVM address to mainnet address</H2>
-        <P>
-          If you have an Metamask EVM address (e.g. 0x1234...), you can use this tool to convert it
-          into a mainnet address (e.g. i76Ux...) to use with polkadot-js.
-        </P>
-
-        <AddressConverter type="evm-address" />
       </Section>
     </>
   );
