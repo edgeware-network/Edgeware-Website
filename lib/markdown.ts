894ee5a7a8a8c4cc7b1bfc0d9eccd3b9fc641614
Jan Kraus <kraus.jan@gmail.com>
2021-09-09 20:14:41 +0200

    Upgrade all dependencies to lates (with remark breaking changes fixes)

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,7 +1,8 @@
-import remark from 'remark';
-import html from 'remark-html';
+import { remark } from 'remark';
+import remarkHtml from 'remark-html';
 
 export async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await remark().use(remarkHtml).process(markdown);
+
   return result.toString();
 }
