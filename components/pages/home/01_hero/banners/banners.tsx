91b12622ae6bdbcbebc05f0e1f66eaddc9987813
Jan Kraus <16012+johny@users.noreply.github.com>
2021-11-04 10:58:16 +0100

    T-1407 update banners on homepage (#44)

--- components/pages/home/01_hero/banners/banners.tsx
+++ components/pages/home/01_hero/banners/banners.tsx
@@ -3,17 +3,17 @@ import * as React from 'react';
 import styles from './banners.module.scss';
 
 const BANNERS = {
-  bittrex: {
-    href: 'https://medium.com/bittrexglobal/new-listing-edgeware-edg-db01ec89250c',
-    alt: 'Bittex Global Lists Edgeware',
+  kucoin: {
+    href: 'https://www.kucoin.com/news/en-edg-gets-listed-on-kucoin',
+    alt: 'Kucoin Lists Edgeware',
   },
   evm: {
-    href: 'https://blog.edgewa.re/evm-launch-on-edgeware-brings-wealth-of-ethereum-contracts-and-developers-to-substrate/',
-    alt: 'Use EVM on EDG today',
+    href: 'https://docs.edgeware.wiki/development/develop/smart-contracts/evm-smart-contracts',
+    alt: 'EdgeEVM coming soon',
   },
-  coinbase: {
-    href: 'https://ventures.coinbase.com/',
-    alt: 'Coinbase Ventures ❤️ EDG',
+  kabocha: {
+    href: 'https://www.kabocha.network/',
+    alt: 'Kabocha Crowdloan',
   },
   events: {
     href: 'https://calendar.google.com/calendar/u/0/embed?src=commonwealth.im_ihbs892pqubvogtoim6c1hf7ck@group.calendar.google.com',
@@ -34,7 +34,7 @@ export const Banners: React.FC = () => {
               rel="noopener noreferrer"
             >
               <img
-                src={`/images/home/banners/banners-${key}.jpg`}
+                src={`/images/home/banners/banners-${key}.png`}
                 alt={BANNERS[key].alt}
                 width="280"
               />
