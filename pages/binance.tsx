74ef55b7fcbae79d6d4193ac74cb86b7bff29b5d
Jan Kraus <kraus.jan@gmail.com>
2022-04-18 13:53:00 +0100

    Add governance section

--- pages/binance.tsx
+++ pages/binance.tsx
@@ -6,6 +6,7 @@ import { BinanceHero } from 'components/pages/binance/01_hero/binance-hero';
 import { EdgewareIntro } from 'components/pages/binance/02_intro/edgeware-intro';
 import { KabochaIntro } from 'components/pages/binance/03_kabocha/kabocha-intro';
 import { StakeAndEarn } from 'components/pages/binance/04_stake/stake-and-earn';
+import { Governance } from 'components/pages/binance/05_governance/governance';
 
 export default function BinancePage() {
   return (
@@ -22,6 +23,10 @@ export default function BinancePage() {
 
       <Section id="stake" width="very-narrow" gap="none">
         <StakeAndEarn />
+      </Section>
+
+      <Section id="governance" width="very-narrow" gap="standard">
+        <Governance />
       </Section>
     </>
   );
