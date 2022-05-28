8f5edf1962b592f95cadcb1c7dc5bacc9cd430a5
Jan <jan@edgeware.agency>
2022-05-28 20:56:22 +0100

    Tweak homepage platform section

--- components/pages/home/homepage-platform.tsx
+++ components/pages/home/homepage-platform.tsx
@@ -1,6 +1,5 @@
 import { Button } from 'components/common/button';
 import { config } from 'data/config';
-import Link from 'next/link';
 import FileList3LineIcon from 'remixicon/icons/Document/file-list-3-line.svg';
 import DiscordFillIcon from 'remixicon/icons/Logos/discord-fill.svg';
 
@@ -11,9 +10,9 @@ export const HomepagePlatform = () => {
         <div className="h-32 bg-gradient-to-t from-transparent to-grey-900" />
 
         {/* Intro */}
-        <div className="container mx-auto max-w-xl text-center">
-          <h2 className="text-5xl font-medium">Substrate Development Platform</h2>
-          <p className="my-4 text-lg">
+        <div className="container mx-auto text-center">
+          <h2 className="text-6xl font-medium">Substrate Development Platform</h2>
+          <p className="my-8 mx-auto max-w-2xl text-lg">
             Edgeware uses the same powerful framework as Kusama and Polkadot, but features three
             ways to build your dapp or project, including familiar EVM, cutting-edge WASM and
             modular, runtime-level Pallets.
@@ -21,86 +20,72 @@ export const HomepagePlatform = () => {
         </div>
 
         {/* 3 column benefits */}
-        <div className="container mx-auto my-12 max-w-5xl">
+        <div className="container mx-auto my-24 max-w-5xl">
           <div className="flex space-x-16">
             {/* EdgeEVM */}
-            <div className="flex w-1/3 flex-col justify-between rounded-lg bg-grey-700 px-3 py-3 text-center">
-              <div>
-                <h3 className="my-2 text-2xl font-normal text-primary-500">EdgeEVM</h3>
-                <p>
-                  Permissionless, familiar, and Solidity-based, EdgeEVM makes it easy to expand to
-                  new chains, debug and port your dapps, and gives you access to the largest set of
-                  tools and resources.
-                </p>
-              </div>
+            <div className="flex w-1/3 rounded-lg border border-grey-800 bg-[url('/images/home/platform/pink-block-bg.jpg')] bg-cover bg-center bg-no-repeat p-3 text-center">
+              <div className="flex h-full flex-col justify-between rounded-lg bg-grey-700 p-3">
+                <div>
+                  <h3 className="my-4 text-2xl font-normal text-primary-500">EdgeEVM</h3>
+                  <p className="text-lg">
+                    Permissionless, familiar, and Solidity-based, EdgeEVM makes it easy to expand to
+                    new chains, debug and port your dapps, and gives you access to the largest set
+                    of tools and resources.
+                  </p>
+                </div>
 
-              <ul className="mt-12 flex flex-col space-y-2">
-                <li className="rounded-md bg-primary-500 p-1.5 text-center uppercase text-grey-900">
-                  Mature Ecosystem
-                </li>
-                <li className="rounded-md bg-primary-500 p-1.5 text-center uppercase text-grey-900">
-                  Solidity Compatible
-                </li>
-                <li className="rounded-md bg-primary-500 p-1.5 text-center uppercase text-grey-900">
-                  Easy Portability
-                </li>
-                <li className="rounded-md bg-primary-500 p-1.5 text-center uppercase text-grey-900">
-                  Meta Transaction Ready
-                </li>
-              </ul>
+                <ul className="flex flex-col space-y-2 text-center uppercase text-grey-900">
+                  <li className="rounded-md bg-primary-500 p-1.5">Mature Ecosystem</li>
+                  <li className="rounded-md bg-primary-500 p-1.5">Solidity Compatible</li>
+                  <li className="rounded-md bg-primary-500 p-1.5">Easy Portability</li>
+                  <li className="rounded-md bg-primary-500 p-1.5">Meta Transaction Ready</li>
+                </ul>
+              </div>
             </div>
 
             {/* EdgeWASM */}
-            <div className="flex w-1/3 flex-col justify-between rounded-lg bg-grey-700 px-3 py-3 text-center">
-              <div>
-                <h3 className="my-2 text-2xl font-normal text-green-500">EdgeWASM</h3>
-                <p>
-                  The permissionless environment for the experts seeking more performance and
-                  future-proof scalability compared to EdgeEVM.
-                </p>
-              </div>
+            <div className="flex w-1/3 rounded-lg border border-grey-800 bg-[url('/images/home/platform/green-block-bg.jpg')] bg-cover bg-center bg-no-repeat p-3 text-center">
+              <div className="flex h-full flex-col justify-between rounded-lg bg-grey-700 p-3">
+                <div>
+                  <h3 className="my-4 text-2xl font-normal text-green-500">EdgeWASM</h3>
+                  <p className="text-lg">
+                    The permissionless environment for the experts seeking more performance and
+                    future-proof scalability compared to EdgeEVM.
+                  </p>
+                </div>
 
-              <ul className="mt-12 flex flex-col space-y-2">
-                <li className="rounded-md bg-green-500 p-1.5 text-center uppercase text-grey-900">
-                  High Performing Permissionless Environment
-                </li>
-                <li className="rounded-md bg-green-500 p-1.5 text-center uppercase text-grey-900">
-                  Memory/Weight Efficient
-                </li>
-                <li className="rounded-md bg-green-500 p-1.5 text-center uppercase text-grey-900">
-                  Futureproof
-                </li>
-                <li className="rounded-md bg-green-500 p-1.5 text-center uppercase text-grey-900">
-                  Rust Ink
-                </li>
-              </ul>
+                <ul className="mt-12 flex flex-col space-y-2 text-center uppercase text-grey-900">
+                  <li className="rounded-md bg-green-500 p-1.5">
+                    High Performing Permissionless Environment
+                  </li>
+                  <li className="rounded-md bg-green-500 p-1.5">Memory/Weight Efficient</li>
+                  <li className="rounded-md bg-green-500 p-1.5">Futureproof</li>
+                  <li className="rounded-md bg-green-500 p-1.5">Rust Ink</li>
+                </ul>
+              </div>
             </div>
 
             {/* Ecosystem */}
-            <div className="flex w-1/3 flex-col justify-between rounded-lg bg-grey-700 px-3 py-3 text-center">
-              <div>
-                <h3 className="my-2 text-2xl font-normal text-grey-300">SubstratePallets</h3>
-                <p>
-                  Unlock the full power and performance of the runtime using Substrate Pallets.
-                  Fully interoperable and future-proof, Rust-based pallets can grant the chain new
-                  functions once approved by the governing community.
-                </p>
-              </div>
+            <div className="flex w-1/3 rounded-lg border border-grey-800 bg-[url('/images/home/platform/grey-block-bg.jpg')] bg-cover bg-center bg-no-repeat p-3 text-center">
+              <div className="flex h-full flex-col justify-between rounded-lg bg-grey-700 p-3">
+                <div>
+                  <h3 className="my-4 text-2xl font-normal text-grey-300">SubstratePallets</h3>
+                  <p className="text-lg">
+                    Unlock the full power and performance of the runtime using Substrate Pallets.
+                    Fully interoperable and future-proof, Rust-based pallets can grant the chain new
+                    functions once approved by the governing community.
+                  </p>
+                </div>
 
-              <ul className="mt-12 flex flex-col space-y-2">
-                <li className="rounded-md bg-grey-300 p-1.5 text-center uppercase text-grey-900">
-                  Interoperability with other Pallets
-                </li>
-                <li className="rounded-md bg-grey-300 p-1.5 text-center uppercase text-grey-900">
-                  Low Level Access
-                </li>
-                <li className="rounded-md bg-grey-300 p-1.5 text-center uppercase text-grey-900">
-                  Permissioned Deployment
-                </li>
-                <li className="rounded-md bg-grey-300 p-1.5 text-center uppercase text-grey-900">
-                  Forkless Upgrades
-                </li>
-              </ul>
+                <ul className="mt-12 flex flex-col space-y-2 text-center uppercase text-grey-900">
+                  <li className="rounded-md bg-grey-300 p-1.5">
+                    Interoperability with other Pallets
+                  </li>
+                  <li className="rounded-md bg-grey-300 p-1.5">Low Level Access</li>
+                  <li className="rounded-md bg-grey-300 p-1.5">Permissioned Deployment</li>
+                  <li className="rounded-md bg-grey-300 p-1.5">Forkless Upgrades</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -114,7 +99,7 @@ export const HomepagePlatform = () => {
         </div>
 
         {/* Docs links */}
-        <div className="container mx-auto my-8 max-w-4xl">
+        <div className="container mx-auto my-16 max-w-4xl">
           <ul className="flex flex-row space-x-4">
             <li className="w-1/2">
               <a
@@ -155,8 +140,8 @@ export const HomepagePlatform = () => {
             </li>
           </ul>
 
-          <div className="my-8 text-center">
-            <Button href="/developers" sizing="normal" colorStyle="primary">
+          <div className="my-16 text-center">
+            <Button href="/developers" sizing="large" colorStyle="primary">
               Start Building
             </Button>
           </div>
