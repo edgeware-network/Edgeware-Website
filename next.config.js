3847569ea94887122b85421209c9a10bc028f745
Jan <jan@edgeware.agency>
2022-06-27 08:49:21 +0100

    Configure redirects

--- next.config.js
+++ next.config.js
@@ -3,4 +3,23 @@ const withSvgr = require('next-svgr');
 module.exports = withSvgr({
   reactStrictMode: false, // <- See https://github.com/tailwindlabs/headlessui/issues/681
   generateBuildId: async () => 'current',
+  redirects: async () => {
+    return [
+      {
+        source: '/partners',
+        destination: '/ecosystem',
+        permanent: true,
+      },
+      {
+        source: '/collectives',
+        destination: '/society',
+        permanent: true,
+      },
+      {
+        source: '/developers',
+        destination: '/contribute',
+        permanent: true,
+      },
+    ];
+  },
 });
