46fb8539bea9b6589ffafb8b531e8a9e24eb075a
Jan <jan@edgeware.agency>
2022-07-13 23:57:09 +0100

    Rewrite society intro

--- components/pages/society/society-intro.tsx
+++ components/pages/society/society-intro.tsx
@@ -3,8 +3,9 @@ export const SocietyIntro = () => {
     <section id="edgeware-society" className="my-24 px-4 text-center">
       <h1 className="my-8 text-6xl">Edgeware Society</h1>
       <p className="my-4 mx-auto max-w-prose text-lg leading-relaxed">
-        A collective is a group of entities that share or are motivated by at least one common issue
-        or interest, or work together to achieve a common objective.
+        At Edgeware society is organized in collectives, a group of entities that share or are
+        motivated by at least one common issue or interest, or work together to achieve a common
+        objective.
       </p>
     </section>
   );
