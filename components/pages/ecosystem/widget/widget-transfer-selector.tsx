8c1a4eec8285d65f2ac8d67d3416c9d6a6a0cb9b
Jan <jan@edgeware.agency>
2023-03-09 17:46:15 +0000

    Improve transfer animation

--- components/pages/ecosystem/widget/widget-transfer-selector.tsx
+++ components/pages/ecosystem/widget/widget-transfer-selector.tsx
@@ -14,6 +14,10 @@ export const WidgetTransferSelector = ({
   const handleChange = (e: React.MouseEvent) => {
     e.preventDefault();
 
+    if (inProgress) {
+      return;
+    }
+
     if (targetTransferType === 'deposit') {
       onTargetTransferTypeChange('withdrawal');
     } else {
@@ -22,19 +26,28 @@ export const WidgetTransferSelector = ({
   };
 
   return (
-    <div className="space-between flex w-full flex-row items-center justify-around">
-      <span className="w-48 p-2 text-xl">EdgeWASM</span>
-      <button
-        className={`rounded-full bg-white p-4 ${inProgress ? 'animate-pulse' : ''}`}
-        onClick={handleChange}
-      >
-        <IconArrow
-          className={`h-12 w-12 fill-primary-600 ${
-            targetTransferType === 'deposit' ? 'rotate-0' : 'rotate-180'
-          } transition-transform`}
-        />
-      </button>
-      <span className="w-48 p-2 text-xl">EdgeEVM</span>
+    <div className="space-between relative flex w-full flex-row items-center justify-around">
+      <span className="w-48 shrink-0 p-2 text-xl">EdgeWASM</span>
+      <div className="flex flex-col items-center">
+        <button
+          className={`relative mb-2 w-20 grow-0 rounded-full bg-white p-4 ${
+            inProgress ? 'animate-pulse' : ''
+          }`}
+          onClick={handleChange}
+        >
+          <IconArrow
+            className={`h-12 w-12 fill-primary-600 ${
+              targetTransferType === 'deposit' ? 'rotate-0' : 'rotate-180'
+            } transition-transform`}
+          />
+        </button>
+        <span
+          className={`w-48 text-center text-xs text-grey-500 ${inProgress ? 'animate-pulse' : ''}`}
+        >
+          {inProgress ? 'Transfer in progress...' : 'Click to change direction'}
+        </span>
+      </div>
+      <span className="w-48 shrink-0 p-2 text-xl">EdgeEVM</span>
     </div>
   );
 };
