427dd00b4398bfcffc1f44e62b787134526e7500
Jan <jan@edgeware.agency>
2023-03-16 17:45:16 +0000

    Improve layout / column centering

--- components/pages/ecosystem/widget/widget-transfer-selector.tsx
+++ components/pages/ecosystem/widget/widget-transfer-selector.tsx
@@ -26,28 +26,32 @@ export const WidgetTransferSelector = ({
   };
 
   return (
-    <div className="space-between relative flex w-full flex-row items-center justify-around">
-      <span className="w-48 shrink-0 p-2 text-xl">EdgeWASM</span>
-      <div className="flex flex-col items-center">
-        <button
-          className={`relative mb-2 w-20 grow-0 rounded-full bg-white p-4 ${
-            inProgress ? 'animate-pulse' : ''
-          }`}
-          onClick={handleChange}
-        >
-          <IconArrow
-            className={`h-12 w-12 fill-primary-600 ${
-              targetTransferType === 'deposit' ? 'rotate-0' : 'rotate-180'
-            } transition-transform`}
-          />
-        </button>
-        <span
-          className={`w-48 text-center text-xs text-grey-500 ${inProgress ? 'animate-pulse' : ''}`}
-        >
-          {inProgress ? 'Transfer in progress...' : 'Click to change direction'}
-        </span>
+    <div className="relative grid h-24 grid-cols-2">
+      <div className="mr-8 mt-4 p-2 text-center text-xl">EdgeWASM</div>
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+        <div className="flex flex-col items-center">
+          <button
+            className={`relative mb-2 w-20 grow-0 rounded-full bg-white p-4 ${
+              inProgress ? 'animate-pulse' : ''
+            }`}
+            onClick={handleChange}
+          >
+            <IconArrow
+              className={`h-12 w-12 fill-primary-600 ${
+                targetTransferType === 'deposit' ? 'rotate-0' : 'rotate-180'
+              } transition-transform`}
+            />
+          </button>
+          <span
+            className={`w-48 text-center text-xs text-grey-500 ${
+              inProgress ? 'animate-pulse' : ''
+            }`}
+          >
+            {inProgress ? 'Transfer in progress...' : 'Click to change direction'}
+          </span>
+        </div>
       </div>
-      <span className="w-48 shrink-0 p-2 text-xl">EdgeEVM</span>
+      <div className="ml-8 mt-4 p-2 text-center text-xl">EdgeEVM</div>
     </div>
   );
 };
