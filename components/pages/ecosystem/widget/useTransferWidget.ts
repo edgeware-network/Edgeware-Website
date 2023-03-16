ac16e168248d7d4cae7255d17289569f28074671
Jan <jan@edgeware.agency>
2023-03-16 17:34:45 +0000

    Refactor widget logic to hook

---
+++ components/pages/ecosystem/widget/useTransferWidget.ts
@@ -0,0 +1,330 @@
+import { useReducer, useRef, useEffect } from 'react';
+import { useWeb3Context } from './web3-context';
+import { validateEVMAddress, validateSubstrateAddress, validateTokenAmount } from 'lib/crypto';
+import { processEVMDeposit } from 'lib/crypto/deposit';
+import { processEVMWithdrawal } from 'lib/crypto/withdrawal';
+
+// Shared types
+type FormState = 'initial' | 'ready' | 'in-progress' | 'success' | 'error';
+type TargetTransferType = 'deposit' | 'withdrawal';
+type FormErrorsState = {
+  global?: string;
+  substrateAddress?: string;
+  evmAddress?: string;
+  amount?: string;
+};
+
+// State
+type State = {
+  formState: FormState;
+  ethereumConnected: boolean;
+  polkadotConnected: boolean;
+  targetTransferType: TargetTransferType;
+  selectedPolkadotAccount: string | undefined;
+  selectedEthereumAccount: string | undefined;
+  errors: FormErrorsState;
+  tx: string | null;
+};
+
+// Initial state
+const initialState: State = {
+  formState: 'initial',
+  ethereumConnected: false,
+  polkadotConnected: false,
+  targetTransferType: 'deposit',
+  selectedPolkadotAccount: undefined,
+  selectedEthereumAccount: undefined,
+  errors: {},
+  tx: null,
+};
+
+// Action types
+type Action =
+  | { type: 'SET_FORM_STATE'; payload: FormState }
+  | { type: 'SET_ETHEREUM_CONNECTED'; payload: boolean }
+  | { type: 'SET_POLKADOT_CONNECTED'; payload: boolean }
+  | { type: 'SET_TARGET_TRANSFER_TYPE'; payload: TargetTransferType }
+  | { type: 'SET_SELECTED_POLKADOT_ACCOUNT'; payload: string | undefined }
+  | { type: 'SET_SELECTED_ETHEREUM_ACCOUNT'; payload: string | undefined }
+  | { type: 'SET_ERRORS'; payload: FormErrorsState }
+  | { type: 'SET_TX'; payload: string | null };
+
+// Reducer
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'SET_FORM_STATE':
+      return { ...state, formState: action.payload };
+    case 'SET_ETHEREUM_CONNECTED':
+      return { ...state, ethereumConnected: action.payload };
+    case 'SET_POLKADOT_CONNECTED':
+      return { ...state, polkadotConnected: action.payload };
+    case 'SET_TARGET_TRANSFER_TYPE':
+      return { ...state, targetTransferType: action.payload };
+    case 'SET_SELECTED_POLKADOT_ACCOUNT':
+      return { ...state, selectedPolkadotAccount: action.payload };
+    case 'SET_SELECTED_ETHEREUM_ACCOUNT':
+      return { ...state, selectedEthereumAccount: action.payload };
+    case 'SET_ERRORS':
+      return { ...state, errors: action.payload };
+    case 'SET_TX':
+      return { ...state, tx: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const useTransferWidget = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const amountInputRef = useRef<HTMLInputElement>(null);
+  const { connectToEthereum, ethereumAccounts, connectToPolkadot, polkadotAccounts } =
+    useWeb3Context();
+
+  // Actions
+  const setFormState = (formState: FormState) =>
+    dispatch({ type: 'SET_FORM_STATE', payload: formState });
+  const setEthereumConnected = (connected: boolean) =>
+    dispatch({ type: 'SET_ETHEREUM_CONNECTED', payload: connected });
+  const setPolkadotConnected = (connected: boolean) =>
+    dispatch({ type: 'SET_POLKADOT_CONNECTED', payload: connected });
+  const setTargetTransferType = (transferType: TargetTransferType) =>
+    dispatch({ type: 'SET_TARGET_TRANSFER_TYPE', payload: transferType });
+  const setSelectedPolkadotAccount = (account: string | undefined) =>
+    dispatch({ type: 'SET_SELECTED_POLKADOT_ACCOUNT', payload: account });
+  const setSelectedEthereumAccount = (account: string | undefined) =>
+    dispatch({ type: 'SET_SELECTED_ETHEREUM_ACCOUNT', payload: account });
+  const setErrors = (errors: FormErrorsState) => dispatch({ type: 'SET_ERRORS', payload: errors });
+  const setTx = (tx: string | null) => dispatch({ type: 'SET_TX', payload: tx });
+
+  // Logic functions
+  const handleDepositSubmit = () => {
+    // reset state to ready and clear errors
+    setFormState('ready');
+    setErrors({});
+
+    // get data
+    const { selectedPolkadotAccount: substrateAddress, selectedEthereumAccount: evmAddress } =
+      state;
+    const amount = amountInputRef.current?.value;
+
+    // validate substrate address
+    const substrateAddressValidation = validateSubstrateAddress(substrateAddress);
+    if (!substrateAddressValidation.valid) {
+      setErrors({
+        substrateAddress: substrateAddressValidation.message,
+      });
+
+      return;
+    }
+
+    // validate EVM address
+    const evmAddressValidation = validateEVMAddress(evmAddress);
+    if (!evmAddressValidation.valid) {
+      setErrors({
+        evmAddress: evmAddressValidation.message,
+      });
+
+      return;
+    }
+
+    // validate amount
+    const amountValidation = validateTokenAmount(amount);
+    if (!amountValidation.valid) {
+      setErrors({
+        amount: amountValidation.message,
+      });
+
+      return;
+    }
+
+    // submit form and continue with processing
+    setFormState('in-progress');
+
+    async function process() {
+      try {
+        const result = await processEVMDeposit(evmAddress, substrateAddress, amount);
+        if (result.success) {
+          setFormState('success');
+          setTx(result?.data?.tx);
+        }
+      } catch (error) {
+        setErrors({
+          global: error.message,
+        });
+        setFormState('error');
+        return;
+      }
+    }
+
+    process();
+  };
+
+  const handleWithdrawalSubmit = () => {
+    // reset state to ready and clear errors
+    setFormState('ready');
+    setErrors({});
+
+    // get data
+    const { selectedPolkadotAccount: substrateAddress, selectedEthereumAccount: evmAddress } =
+      state;
+    const amount = amountInputRef.current?.value;
+
+    // validate substrate address
+    const substrateAddressValidation = validateSubstrateAddress(substrateAddress);
+    if (!substrateAddressValidation.valid) {
+      setErrors({
+        substrateAddress: substrateAddressValidation.message,
+      });
+
+      return;
+    }
+
+    // validate EVM address
+    const evmAddressValidation = validateEVMAddress(evmAddress);
+    if (!evmAddressValidation.valid) {
+      setErrors({
+        evmAddress: evmAddressValidation.message,
+      });
+
+      return;
+    }
+
+    // validate amount
+    const amountValidation = validateTokenAmount(amount);
+    if (!amountValidation.valid) {
+      setErrors({
+        amount: amountValidation.message,
+      });
+
+      return;
+    }
+
+    // submit form and continue with processing
+    setFormState('in-progress');
+
+    async function process() {
+      try {
+        const result = await processEVMWithdrawal(evmAddress, substrateAddress, amount);
+        if (result.success) {
+          setFormState('success');
+          setTx(result?.data?.tx);
+        }
+      } catch (error) {
+        console.error(error);
+        setErrors({
+          global: error.message,
+        });
+        setFormState('error');
+        return;
+      }
+    }
+
+    process();
+  };
+
+  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (state.targetTransferType === 'deposit') {
+      handleDepositSubmit();
+    } else {
+      handleWithdrawalSubmit();
+    }
+  };
+
+  const handleReset = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (state.polkadotConnected && state.ethereumConnected) {
+      setFormState('ready');
+    } else {
+      setFormState('initial');
+    }
+    setTargetTransferType('deposit');
+    setErrors({});
+    amountInputRef.current.value = '';
+  };
+
+  const handleConnect = (type: 'polkadot' | 'ethereum') => {
+    const isConnected = type === 'polkadot' ? state.polkadotConnected : state.ethereumConnected;
+
+    if (isConnected) {
+      return;
+    }
+
+    async function connect() {
+      if (type === 'polkadot') {
+        const result = await connectToPolkadot();
+        if (result) {
+          setPolkadotConnected(true);
+          if (state.ethereumConnected) {
+            setFormState('ready');
+          }
+        }
+      } else {
+        try {
+          const result = await connectToEthereum();
+          if (result) {
+            setEthereumConnected(true);
+            if (state.polkadotConnected) {
+              setFormState('ready');
+            }
+          }
+        } catch (error) {
+          setErrors({
+            ...state.errors,
+            evmAddress: "Couldn't connect to Ethereum.",
+          });
+        }
+      }
+    }
+
+    connect();
+  };
+
+  const handleDisconnect = (type: 'polkadot' | 'ethereum') => {
+    const isConnected = type === 'polkadot' ? state.polkadotConnected : state.ethereumConnected;
+    if (!isConnected) {
+      return;
+    }
+
+    if (type === 'polkadot') {
+      setPolkadotConnected(false);
+      setSelectedPolkadotAccount(undefined);
+      setFormState('initial');
+    }
+
+    if (type === 'ethereum') {
+      setEthereumConnected(false);
+      setSelectedEthereumAccount(undefined);
+      setFormState('initial');
+    }
+  };
+
+  // useEffect hooks to synchronize hook state with useWeb3Context state
+  useEffect(() => {
+    if (state.polkadotConnected && polkadotAccounts && polkadotAccounts.length > 0) {
+      setSelectedPolkadotAccount(polkadotAccounts[0].address);
+    }
+
+    if (state.ethereumConnected && ethereumAccounts && ethereumAccounts.length > 0) {
+      setSelectedEthereumAccount(ethereumAccounts[0].address);
+    }
+  }, [state, polkadotAccounts, ethereumAccounts]);
+
+  return {
+    state,
+    polkadotAccounts,
+    ethereumAccounts,
+    amountInputRef,
+    setFormState,
+    setEthereumConnected,
+    setPolkadotConnected,
+    setTargetTransferType,
+    setSelectedPolkadotAccount,
+    setSelectedEthereumAccount,
+    setErrors,
+    setTx,
+    handleSubmit,
+    handleReset,
+    handleConnect,
+    handleDisconnect,
+  };
+};
--- components/pages/ecosystem/widget/widget-form.tsx
+++ components/pages/ecosystem/widget/widget-form.tsx
@@ -1,288 +1,58 @@
-import { validateEVMAddress, validateSubstrateAddress, validateTokenAmount } from 'lib/crypto';
-import { processEVMDeposit } from 'lib/crypto/deposit';
-import React, { useState, useEffect, useRef } from 'react';
-import { useWeb3Context } from './web3-context';
-import { processEVMWithdrawal } from 'lib/crypto/withdrawal';
+import { useTransferWidget } from './useTransferWidget';
 import { WidgetTransferSelector } from './widget-transfer-selector';
 import { WidgetWalletSelector } from './widget-wallet-selector';
 
-type FormState = 'initial' | 'ready' | 'in-progress' | 'success' | 'error';
-
-type TargetTransferType = 'deposit' | 'withdrawal';
-
-type FormErrorsState = {
-  global?: string;
-  substrateAddress?: string;
-  evmAddress?: string;
-  amount?: string;
-};
-
 export const WidgetForm = () => {
-  const [formState, setFormState] = useState<FormState>('initial');
-
-  const [ethereumConnected, setEthereumConnected] = useState(false);
-  const [polkadotConnected, setPolkadotConnected] = useState(false);
-  const [targetTransferType, setTargetTransferType] = useState<TargetTransferType>('deposit');
-
-  const [selectedPolkadotAccount, setSelectedPolkadotAccount] = useState<string>();
-  const [selectedEthereumAccount, setSelectedEthereumAccount] = useState<string>();
-
-  const [errors, setErrors] = useState<FormErrorsState>({});
-  const [tx, setTx] = useState<string | null>(null);
-
-  const { connectToEthereum, ethereumAccounts, connectToPolkadot, polkadotAccounts } =
-    useWeb3Context();
-
-  const amountInputRef = useRef<HTMLInputElement>(null);
-
-  const handleDepositSubmit = () => {
-    // reset state to ready and clear errors
-    setFormState('ready');
-    setErrors({});
-
-    // get form data and validate
-    const substrateAddress = selectedPolkadotAccount;
-    const evmAddress = selectedEthereumAccount;
-    const amount = amountInputRef.current?.value;
-
-    // validate substrate address
-    const substrateAddressValidation = validateSubstrateAddress(substrateAddress);
-    if (!substrateAddressValidation.valid) {
-      setErrors({
-        substrateAddress: substrateAddressValidation.message,
-      });
-
-      return;
-    }
-
-    // validate EVM address
-    const evmAddressValidation = validateEVMAddress(evmAddress);
-    if (!evmAddressValidation.valid) {
-      setErrors({
-        evmAddress: evmAddressValidation.message,
-      });
-
-      return;
-    }
-
-    // validate amount
-    const amountValidation = validateTokenAmount(amount);
-    if (!amountValidation.valid) {
-      setErrors({
-        amount: amountValidation.message,
-      });
-
-      return;
-    }
-
-    // submit form and continue with processing
-    setFormState('in-progress');
-
-    async function process() {
-      try {
-        const result = await processEVMDeposit(evmAddress, substrateAddress, amount);
-        if (result.success) {
-          setFormState('success');
-          setTx(result?.data?.tx);
-        }
-      } catch (error) {
-        setErrors({
-          global: error.message,
-        });
-        setFormState('error');
-        return;
-      }
-    }
-
-    process();
-  };
-
-  const handleWithdrawalSubmit = () => {
-    // reset state to ready and clear errors
-    setFormState('ready');
-    setErrors({});
-
-    // get form data and validate
-    const substrateAddress = selectedPolkadotAccount;
-    const evmAddress = selectedEthereumAccount;
-    const amount = amountInputRef.current?.value;
-
-    // validate substrate address
-    const substrateAddressValidation = validateSubstrateAddress(substrateAddress);
-    if (!substrateAddressValidation.valid) {
-      setErrors({
-        substrateAddress: substrateAddressValidation.message,
-      });
-
-      return;
-    }
-
-    // validate EVM address
-    const evmAddressValidation = validateEVMAddress(evmAddress);
-    if (!evmAddressValidation.valid) {
-      setErrors({
-        evmAddress: evmAddressValidation.message,
-      });
-
-      return;
-    }
-
-    // validate amount
-    const amountValidation = validateTokenAmount(amount);
-    if (!amountValidation.valid) {
-      setErrors({
-        amount: amountValidation.message,
-      });
-
-      return;
-    }
-
-    // submit form and continue with processing
-    setFormState('in-progress');
-
-    async function process() {
-      try {
-        const result = await processEVMWithdrawal(evmAddress, substrateAddress, amount);
-        if (result.success) {
-          setFormState('success');
-          setTx(result?.data?.tx);
-        }
-      } catch (error) {
-        console.error(error);
-        setErrors({
-          global: error.message,
-        });
-        setFormState('error');
-        return;
-      }
-    }
-
-    process();
-  };
-
-  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    if (targetTransferType === 'deposit') {
-      handleDepositSubmit();
-    } else {
-      handleWithdrawalSubmit();
-    }
-  };
-
-  const handleReset = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    if (polkadotConnected && ethereumConnected) {
-      setFormState('ready');
-    } else {
-      setFormState('initial');
-    }
-    setTargetTransferType('deposit');
-    setErrors({});
-    amountInputRef.current.value = '';
-  };
-
-  const handleConnect = (type: 'polkadot' | 'ethereum') => {
-    const isConnected = type === 'polkadot' ? polkadotConnected : ethereumConnected;
-
-    if (isConnected) {
-      return;
-    }
-
-    async function connect() {
-      if (type === 'polkadot') {
-        const result = await connectToPolkadot();
-        if (result) {
-          setPolkadotConnected(true);
-          if (ethereumConnected) {
-            setFormState('ready');
-          }
-        }
-      } else {
-        try {
-          const result = await connectToEthereum();
-          if (result) {
-            setEthereumConnected(true);
-            if (polkadotConnected) {
-              setFormState('ready');
-            }
-          }
-        } catch (error) {
-          setErrors({
-            ...errors,
-            evmAddress: "Couldn't connect to Ethereum.",
-          });
-        }
-      }
-    }
-
-    connect();
-  };
-
-  const handleDisconnect = (type: 'polkadot' | 'ethereum') => {
-    const isConnected = type === 'polkadot' ? polkadotConnected : ethereumConnected;
-    if (!isConnected) {
-      return;
-    }
-
-    if (type === 'polkadot') {
-      setPolkadotConnected(false);
-      setSelectedPolkadotAccount(undefined);
-      setFormState('initial');
-    }
-
-    if (type === 'ethereum') {
-      setEthereumConnected(false);
-      setSelectedEthereumAccount(undefined);
-      setFormState('initial');
-    }
-  };
-
-  useEffect(() => {
-    if (polkadotConnected && polkadotAccounts && polkadotAccounts.length > 0) {
-      setSelectedPolkadotAccount(polkadotAccounts[0].address);
-    }
-  }, [polkadotConnected, polkadotAccounts]);
-
-  useEffect(() => {
-    if (ethereumConnected && ethereumAccounts && ethereumAccounts.length > 0) {
-      setSelectedEthereumAccount(ethereumAccounts[0].address);
-    }
-  }, [ethereumConnected, ethereumAccounts]);
+  const {
+    state,
+    amountInputRef,
+    setTargetTransferType,
+    handleConnect,
+    handleDisconnect,
+    handleSubmit,
+    handleReset,
+    polkadotAccounts,
+    ethereumAccounts,
+    setSelectedPolkadotAccount,
+    setSelectedEthereumAccount,
+  } = useTransferWidget();
 
   return (
     <div className="text-center">
       <WidgetTransferSelector
-        targetTransferType={targetTransferType}
+        targetTransferType={state.targetTransferType}
         onTargetTransferTypeChange={setTargetTransferType}
-        inProgress={formState === 'in-progress'}
+        inProgress={state.formState === 'in-progress'}
       />
 
       <div className="flex flex-row items-start justify-center space-x-16 px-8 pt-12">
         <div className="w-1/2 shrink-0">
           <WidgetWalletSelector
             type="polkadot"
-            connected={polkadotConnected}
+            connected={state.polkadotConnected}
             onConnect={handleConnect}
             onDisconnect={handleDisconnect}
             accounts={polkadotAccounts}
-            selectedAccount={selectedPolkadotAccount}
+            selectedAccount={state.selectedPolkadotAccount}
             setSelectedAccount={setSelectedPolkadotAccount}
           />
-          {errors.substrateAddress && (
-            <span className="text-sm text-red-500">{errors.substrateAddress}</span>
+          {state.errors.substrateAddress && (
+            <span className="text-sm text-red-500">{state.errors.substrateAddress}</span>
           )}
         </div>
         <div className="w-1/2 shrink-0">
           <WidgetWalletSelector
             type="ethereum"
-            connected={ethereumConnected}
+            connected={state.ethereumConnected}
             onConnect={handleConnect}
             onDisconnect={handleDisconnect}
             accounts={ethereumAccounts}
-            selectedAccount={selectedEthereumAccount}
+            selectedAccount={state.selectedEthereumAccount}
             setSelectedAccount={setSelectedEthereumAccount}
           />
-          {errors.evmAddress && <span className="text-sm text-red-500">{errors.evmAddress}</span>}
+          {state.errors.evmAddress && (
+            <span className="text-sm text-red-500">{state.errors.evmAddress}</span>
+          )}
         </div>
       </div>
 
@@ -297,12 +67,14 @@ export const WidgetForm = () => {
             required
             placeholder="Amount (EDG)"
             ref={amountInputRef}
-            disabled={formState !== 'ready'}
+            disabled={state.formState !== 'ready'}
             autoComplete="off"
             autoCapitalize="none"
             autoCorrect="off"
           />
-          {errors.amount && <span className="text-sm text-red-500">{errors.amount}</span>}
+          {state.errors.amount && (
+            <span className="text-sm text-red-500">{state.errors.amount}</span>
+          )}
         </label>
       </div>
 
@@ -310,24 +82,24 @@ export const WidgetForm = () => {
         <button
           className="rounded bg-primary-500 p-4 px-8 text-white disabled:opacity-25"
           onClick={handleSubmit}
-          disabled={formState !== 'ready'}
+          disabled={state.formState !== 'ready'}
         >
           Transfer
         </button>
       </div>
 
-      {formState === 'error' ? (
+      {state.formState === 'error' ? (
         <p className="pt-4 text-red-500">
-          {errors.global || 'Something went wrong. Please try again.'}
+          {state.errors.global || 'Something went wrong. Please try again.'}
         </p>
       ) : null}
 
-      {formState === 'success' && tx && (
+      {state.formState === 'success' && state.tx && (
         <div className="pt-8">
           Transfer submitted!
           <div className="mt-2 flex justify-center">
             <a
-              href={`https://edgeware.subscan.io/extrinsic/${tx}`}
+              href={`https://edgeware.subscan.io/extrinsic/${state.tx}`}
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-500 ml-2 rounded bg-grey-400 p-2 px-4 text-sm"
@@ -337,7 +109,7 @@ export const WidgetForm = () => {
             <button
               className="text-blue-500 ml-2 rounded bg-grey-400 p-2 px-4 text-sm"
               onClick={handleReset}
-              disabled={formState !== 'success'}
+              disabled={state.formState !== 'success'}
             >
               Start again
             </button>
@@ -345,7 +117,9 @@ export const WidgetForm = () => {
         </div>
       )}
 
-      {formState === 'in-progress' && <p className="animate-pulse pt-4">Processing request...</p>}
+      {state.formState === 'in-progress' && (
+        <p className="animate-pulse pt-4">Processing request...</p>
+      )}
     </div>
   );
 };
