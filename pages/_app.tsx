52359eb5c7a2b85e06db72779c72237713d59e6b
pranavcpawar <pranavcpawar@outlook.com>
2023-11-15 01:10:06 +0530

    add thirdweb

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import { ThirdwebProvider, localWallet, metamaskWallet } from '@thirdweb-dev/react';
+import { EdgewareEdgeevm } from '@thirdweb-dev/chains';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import * as gtag from '../utils/gtag';
@@ -28,7 +30,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout meta={pageProps.meta} {...pageProps.layout} currentPath={router.pathname}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        <ThirdwebProvider activeChain={{...EdgewareEdgeevm, rpc: ['https://edgeware-evm.jelliedowl.net']}} clientId='a768a5d757fbd7a552b1a6c5749039ee' supportedWallets={[metamaskWallet(), localWallet()]}>
+          <Component {...pageProps} />
+        </ThirdwebProvider>
       </QueryClientProvider>
     </Layout>
   );
