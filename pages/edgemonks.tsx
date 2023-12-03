9df14030526fc724eff6ba5aeb092d54716ff913
Shankar D. Warang <shankar@edgeware.agency>
2023-12-04 01:46:28 +0530

    minor change

--- pages/edgemonks.tsx
+++ pages/edgemonks.tsx
@@ -5,7 +5,7 @@ import IconArrow from 'remixicon/icons/Arrows/arrow-down-s-line.svg';
 const WidgetCampaign = [
   {
     index: 1,
-    campaign: 'EdgeMonk',
+    campaign: 'EdgeMonk Genesis',
     src: 'https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0x83F589Cd9549483c3D962f09afdd4CC492617787&chain=%7B%22name%22%3A%22Edgeware+EdgeEVM+Mainnet%22%2C%22chain%22%3A%22EDG%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fedgeware-edgeevm.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Edgeware%22%2C%22symbol%22%3A%22EDG%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22edg%22%2C%22chainId%22%3A2021%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22edgeware-edgeevm%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmS3ERgAKYTmV7bSWcUPSvrrCC9wHQYxtZqEQYx9Rw4RGA%22%2C%22width%22%3A352%2C%22height%22%3A304%2C%22format%22%3A%22png%22%7D%7D&clientId=57b1f3ff173c0acef018fbca1356a7af&theme=dark&primaryColor=green',
   },
 ];
