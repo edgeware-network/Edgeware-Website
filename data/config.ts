971fa709b663bfc5f1672fcdcc4e2530ec4f2247
Shankar D. Warang <shankar@edgeware.community>
2024-05-23 01:07:29 +0530

    update positioning for footer logos

--- data/config.ts
+++ data/config.ts
@@ -2,8 +2,8 @@ export const socialMediaUrls = {
   discordUrl: 'https://discord.gg/4pnzjWTJ9b',
   twitterUrl: 'https://twitter.com/EdgewareDAO',
   githubUrl: 'https://github.com/orgs/Edgeware-Network',
-  redditUrl: 'https://www.reddit.com/r/edgeware',
-  telegramUrl: 'https://t.me/heyedgeware',
-  matrixUrl: 'https://matrix.to/#/#Edgeverse:matrix.org',
   peeranhaUrl: 'https://edgeware.peeranha.io/',
+  telegramUrl: 'https://t.me/heyedgeware',
+  redditUrl: 'https://www.reddit.com/r/edgeware',
+  matrixUrl: 'https://matrix.to/#/#Edgeverse:matrix.org',
 };
